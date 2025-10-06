cd9380d5f0d85d9001d6adbb27d3fc03ac571bd7
TienKhang <nguyentienkhang09022005@gmail.com>
2025-10-06 22:09:13 +0700

    update connect db

---
+++ Personal-Finance-System-BE/Personal-Finance-System-BE/PersonalFinanceSys.Infrastructure/Data/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿namespace Personal_Finance_System_BE.PersonalFinanceSys.Infrastructure.Data.Entities;
+
+public partial class User
+{
+    public Guid IdUser { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Gender { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Password { get; set; }
+
+    public decimal? TotalAmount { get; set; }
+
+    public bool? IsDarkmode { get; set; }
+
+    public DateTime? CreateAt { get; set; }
+}
